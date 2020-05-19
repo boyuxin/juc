88e526720079fff4d6c43104b71a540f5db41ab8
boyuxin <1969591029@qq.com>
2020-05-19 21:15:33 +0800

    第一版本

---
+++ src/com/boyuxin/one/MyFirst.java
@@ -0,0 +1,12 @@
+package com.boyuxin.one;
+
+/**
+ * @author boyuxin
+ * @description
+ * @date 2020/5/19 21:14
+ */
+public class MyFirst {
+    public static void main(String[] args) {
+        System.out.println("hah ");
+    }
+}
